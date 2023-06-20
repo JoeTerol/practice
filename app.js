566480547d2b98b320f106d3e84eeb1cec7c79a4
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-20 20:16:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// // EJERCICIO 1
+// const pregunta = window.prompt("Cuantos años tenes?");
+// if(pregunta >= 18){
+//     alert("eres mayor de edad, ya puedes conducir")
+// }
+
+// EJERCICIO 2
+
