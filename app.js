66ae4e4252e79417eda9d132020b778b7f703aef
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-25 19:47:53 -0300

    new main.js

--- app.js
+++ app.js
@@ -6,3 +6,37 @@
 
 // EJERCICIO 2
 
+
+
+do {
+    const nota = prompt("introduce tu nota");
+
+    if (Number(nota) == nota) {
+        if (nota > 0 && nota <= 10) {
+            if (nota < 3){
+                alert("muy deficiente");
+            }
+            else if(nota < 5) {
+                alert("Insufucuente");
+            } 
+            else if (nota < 6){ 
+                alert("Suficiente");
+            }
+            else if (nota < 7){
+                alert("Bien");
+            }
+            else if (nota <= 8) {
+                alert("Notable");
+            }
+            else if (nota >= 9) {
+                alert("sobresaliente");
+            }
+        }else {
+            alert("ingresa número válido")
+        }
+        
+    }
+}while (nota != undefined);
+
+
+    
