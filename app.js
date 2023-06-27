4616b70ffeabed7dcc92c4414fee2add591ff370
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-27 15:03:06 -0300

    show vocals in document, done

--- app.js
+++ app.js
@@ -8,35 +8,36 @@
 
 
 
-do {
-    const nota = prompt("introduce tu nota");
+// do {
+//     const nota = prompt("introduce tu nota");
 
-    if (Number(nota) == nota) {
-        if (nota > 0 && nota <= 10) {
-            if (nota < 3){
-                alert("muy deficiente");
-            }
-            else if(nota < 5) {
-                alert("Insufucuente");
-            } 
-            else if (nota < 6){ 
-                alert("Suficiente");
-            }
-            else if (nota < 7){
-                alert("Bien");
-            }
-            else if (nota <= 8) {
-                alert("Notable");
-            }
-            else if (nota >= 9) {
-                alert("sobresaliente");
-            }
-        }else {
-            alert("ingresa número válido")
-        }
+//     if (Number(nota) == nota) {
+//         if (nota > 0 && nota <= 10) {
+//             if (nota < 3){
+//                 alert("muy deficiente");
+//             }
+//             else if(nota < 5) {
+//                 alert("Insufucuente");
+//             } 
+//             else if (nota < 6){ 
+//                 alert("Suficiente");
+//             }
+//             else if (nota < 7){
+//                 alert("Bien");
+//             }
+//             else if (nota <= 8) {
+//                 alert("Notable");
+//             }
+//             else if (nota >= 9) {
+//                 alert("sobresaliente");
+//             }
+//         }else {
+//             alert("ingresa número válido")
+//         }
         
-    }
-}while (nota != undefined);
+//     }
+// }while (nota != undefined);
 
+// EJERCICIO 3
 
     
---
+++ main.js
@@ -0,0 +1,41 @@
+// window.alert('Hello World!');
+
+// document.write( 3 + 5 );
+
+// const nombre = window.prompt("Ingrese su nombre de Usuario")
+// alert("hola " + nombre);
+
+//const numero1 = window.prompt("ingresa un numero");
+//const numero2 = window.prompt("ingresa otro número");
+// alert( +numero1 + +numero2);
+
+// const numero1 = window.prompt("ingresa un número");
+// const numero2 = window.prompt("ingresa otro número");
+// const mayor = +numero1 > +numero2 ? numero1 : numero2;
+// alert(mayor)
+
+// const numero1 = window.prompt("ingresa un número");
+// const numero2 = window.prompt("ingresa otro número");
+// const numero3 = window.prompt("ingresa el úlitmo número");
+
+// maximo = Math.max(+numero1, +numero2, +numero3)
+// alert(maximo);
+
+// const numero1 = window.prompt("ingresa un número")
+// if( +numero1 % 2 === 0){
+//     alert("este número es divisble en 2")
+// };
+
+// const frase = window.prompt("Ingrese una Frase");
+
+// const conteo = (frase.match(/a/gi) || []).length;
+
+// document.write("El número  de letras 'a' en la frase es: " + conteo);
+
+// const frase = window.prompt("ingrese una frase");
+
+// const vocales = (frase.match(/[aeiou]/gi));
+
+// document.write("la Frase tiene las sigueintes vocales " + vocales);
+
+
